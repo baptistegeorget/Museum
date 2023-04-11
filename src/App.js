59cf5503bab9f831f47569214189ff2337c2697e
Baptiste Georget <baptiste.georget@hotmail.com>
2023-04-11 16:59:35 +0200

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [objects, setObjects] = useState([])
+  const day = new Date().getDate()
+  const month = new Date().getMonth()
+  const year = new Date().getFullYear()
+
+  useEffect(() => {
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects?metadataDate=${year}-${month}-${day}`)
+        .then((response) => response.json())
+        .then((data) => setObjects(data))
+  }, [])
+
+
+
   return (
     <div>
 
