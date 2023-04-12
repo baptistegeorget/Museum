505f76063fa04e1e0ab71368679cab38c328758f
Baptiste Georget <baptiste.georget@hotmail.com>
2023-04-12 14:43:10 +0200

    Maj searchbar

--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -5,17 +5,17 @@ export function Searchbar() {
 
     const submit = (input) => {
         if (input.key === "Enter" && input.target.value !== "") {
-            
+
         }
     }
 
     return (
-        <form className="row no-gutters mt-3 align-items-center">
-            <div className="col col-md-4">
-                <input className="form-control border-secondary rounded-pill pr-5" id="searchbar" type="search" onKeyUp={submit}/>
+        <form className="row align-items-center">
+            <div className="col-auto">
+                <input className="form-control border-secondary rounded-pill" type="search" placeholder="Search any arts.." onKeyUp={submit}/>
             </div>
             <div className="col-auto">
-                <Search color="black" fontSize="1.5em"/>
+                <Search color="white" fontSize="1.5em"/>
             </div>
         </form>
     )
