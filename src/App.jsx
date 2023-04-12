7ae604709f83a762ac2fd41fca65138ebb9dd5bc
Baptiste Georget <baptiste.georget@hotmail.com>
2023-04-12 17:46:12 +0200

    create search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,51 @@
 import {useEffect, useState} from "react";
 import {Header} from "./components/Header";
-import {BrowserRouter, Outlet, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {OBJECT_URL, SEARCH_URL} from "./config";
 
 function App() {
 
     const [objects, setObjects] = useState([])
 
-    useEffect(() => {
-        fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects`)
+    useEffect(() => search(), [])
+
+    const search = (value = "", limit = 10, departmentId = Infinity, geoLocation = "", dateBegin = Infinity, dateEnd = Infinity, hasImages = false) => {
+        let results = [];
+        let parameters = `?q=${value}`
+        if (departmentId !== Infinity) {
+            parameters += `&departmentId=${departmentId}`
+        }
+        if (geoLocation !== "") {
+            parameters += `&geoLocation=${geoLocation}`
+        }
+        if (dateBegin !== Infinity) {
+            parameters += `&dateBegin=${dateBegin}`
+        }
+        if (dateEnd !== Infinity) {
+            parameters += `&dateEnd=${dateEnd}`
+        }
+        if (hasImages) {
+            parameters += `&hasImages=true`
+        }
+        fetch(`${SEARCH_URL}${parameters}`)
             .then((response) => response.json())
-            .then((data) => setObjects(data))
-    }, [])
+            .then((data) => {
+                if (data.total !== 0) {
+                    for (let i = 0; i < limit; i++) {
+                        fetch(`${OBJECT_URL}${data?.objectIDs[i]}`)
+                            .then((response) => response.json())
+                            .then((data) => results.push(data))
+                    }
+                    setObjects(results)
+                }
+            })
+        console.log(results)
+    }
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Header/>}>
+                <Route path="/" element={<Header search={search}/>}>
                     <Route path="advanced-search" element={<p>Search</p>}/>
                     <Route path="details" element={<p>details</p>}/>
                 </Route>
--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
-const BASE_URL = ""
-
-export {BASE_URL}
+export const SEARCH_URL = "https://collectionapi.metmuseum.org/public/collection/v1/search"
+export const OBJECT_URL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
